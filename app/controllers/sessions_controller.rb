51dcceaa49a21792257df6fcddf48d3077b7e972
Auden Robertson <audenrobertson@gmail.com>
2018-06-10 00:17:34 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,8 @@ class SessionsController < ApplicationController
   end
 
   def create
-    return redirect_to(controller: 'sessions', action: 'new')if !params[:name] || params[:name].empty?
+    return redirect_to(controller: 'sessions',
+      action: 'new')if !params[:name] || params[:name].empty?
       session[:name] = params[:name]
       redirect_to controller: 'application', action: 'hello'
   end
@@ -13,5 +14,4 @@ class SessionsController < ApplicationController
     session.delete :name
     redirect_to controller: 'application', action: 'hello'
   end
-
 end
